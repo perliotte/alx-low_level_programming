e7f1e149e9270bad4adb7867195fc440ff360274
Assia <echbassia@gmail.com>
2023-03-17 13:26:23 -0700

    h

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,9 @@ int main(void)
 	long long int d;
 	float f;
 printf("Size of a char: %1u byte(s)\n", (unsigned long) sizeof(a));
-printf("Size of an int : %1u byte(s)\n", (unsigned long) sizeof(b));
-printf("Size of a long int : %1u byte(s)\n", (unsigned long) sizeof(c));
-printf("Size of a long long int : %1u byte(s)\n", (unsigned long) sizeof(d));
-printf("Size of a float : %1u byte(s)\n", (unsigned long) sizeof(f));
-return(0);
+printf("Size of an int: %1u byte(s)\n", (unsigned long) sizeof(b));
+printf("Size of a long int: %1u byte(s)\n", (unsigned long) sizeof(c));
+printf("Size of a long long int: %1u byte(s)\n", (unsigned long) sizeof(d));
+printf("Size of a float: %1u byte(s)\n", (unsigned long) sizeof(f));
+return (0);
 }
